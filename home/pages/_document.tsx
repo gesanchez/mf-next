da06caa75d12a1dd33d7d3544f2b0b19f38ba970
German Sanchez <germansanchez@MacBook-Pro-de-everis.local>
2021-08-08 17:21:30 -0500

    Microapp con inital fetching

--- home/pages/_document.tsx
+++ home/pages/_document.tsx
@@ -3,18 +3,25 @@ import Document, { Html, Head, Main, NextScript, DocumentContext } from "next/do
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
+    const mf = await Promise.resolve([{ name: 'checkout', url: 'http://localhost:3000/_next/static/chunks/remoteEntry.js'}]);
+    return { microapps: mf, ...initialProps };
   }
 
   render() {
+    const { microapps } = this.props;
     return (
       <Html>
         <Head />
         <body>
-          <script
-            data-webpack="checkout"
-            src="http://localhost:3000/_next/static/chunks/remoteEntry.js"
-          />
+          { microapps.map((element) => {
+            return (
+              <script
+                key={element.name}
+                data-webpack={element.name}
+                src={element.url}
+              />
+            );
+          })}
           <Main />
           <NextScript />
         </body>
